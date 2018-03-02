9358a33d25ec0ae98709ae17597cb04af3ed348e
forsooth <maastntes@gmail.com>
2018-03-01 23:18:32 -0500

    Adding lab work

--- Lab04/Handout/geometry.h
+++ Lab04/Handout/geometry.h
@@ -1,60 +1,54 @@
-/*  =================== File Information =================
-	File Name: geometry.h
-	Description:
-	Author: Michael Shah
-
-	Purpose:
-	Examples:
-	===================================================== */
-
 #ifndef GEOMETRY_H
 #define GEOMETRY_H
 
 /*  ============== Vertex ==============
 	Purpose: Stores properties of each vertex
-	Use: Each vertex is used in face structure.
-
-	Notes: You may not have all of the data available
-	below.  In this case, you would want to make an
-	optimization, and only store (x,y,z) cooridinates for example.
+	Use: Used in face structure
 	==================================== */  
-struct vertex{
-	// position in 3D space
-	float x,y,z;		
-	// I believe this is used to determine if a vertex can be removed.
-	// Search for polygon decimation or vertex removal for more ideas.
-	// source: graphics.standford.edu/software/vrip/plyusage.html
-	float confidence;	
-	// I believe this has to do with lighting and shading, and this value
-	// determines the shading coefficent used. This can be useful for when
-	// we color the model.
-	// source: www.okino.com/conv/imp_ply.htm
+class vertex{
+public:
+	float x,y,z;		// position in 3D space
+	float confidence;
 	float intensity;
-	// Color values
-	float r,g,b;	
-	// surface normal values	
-	float nx,ny,nz;		
-	// texture coordinates
-	float u,v,w;		
+	float r,g,b;		// Color values
 };
 
 /*  ============== Face ==============
 	Purpose: Store list of vertices that make up a polygon.
-			In modern versions of OpenGL this value will always be 3(a triangle)
+	In modern versions of OpenGL this value will always be 3(a triangle)
 	Use: Used in Shape data structure.
 	==================================== */  
-struct face{
-	// The number of vertices that make up a single face.
-	// A face can have anywhere from 3 to n vertices.
-	int vertexCount;
-	// Stores an index(integer value) list of vertices. See struct 'vertex' for more information
-	int* vertexList;
+class face{
+public:
+	int vertexCount;  // assuming 3 for this
+	int* vertexList;  // assuming this has size 3
+  
+        //normal vector
+        float normX, normY, normZ;
+        //dot of normal and look
+        float dotProd;
 
-	// Default constructor
+        // Default constructor
 	face(){
 		vertexCount = 0;
-		vertexList = NULL;
 	}
+
 };
 
+/* Edge: Connects two vertices, and two faces. 
+ */
+class edge{
+public:
+        int vertices[2];
+        int faces[2];
+        
+        //default constructor
+        edge(){
+            //these are -1 because 0 would be a meaningful value
+			vertices[0] = -1;
+			vertices[1] = -1;
+            faces[0] = -1;
+            faces[1] = -1;
+        }
+};
 #endif
